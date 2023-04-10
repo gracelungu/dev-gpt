c29bccf06947005afe4fa0c30e960861d01057ff
GPT-4 <gpt-4@openai.com>
2023-04-10 19:04:34 +0200

    Fix proxy

--- src/pages/api/command.ts
+++ src/pages/api/command.ts
@@ -1,24 +1,24 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import fetch, { Headers } from 'node-fetch';
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 const proxy = async (req: NextApiRequest, res: NextApiResponse) => {
   // Replace this URL with the target URL you want to proxy requests to
-  const targetURL = 'https://wblo235iab.execute-api.eu-north-1.amazonaws.com/prod/command';
+  const targetURL =
+    "https://wblo235iab.execute-api.eu-north-1.amazonaws.com/prod/command";
 
   try {
     // Forward the request to the target URL including the path
-    const response = await fetch(targetURL, {
-      method: req.method,
-      body: req.body,
-    });
+    const response = await axios.post(targetURL, req.body);
 
-    console.log(response.body);
+    console.log(response.data);
+
+    return res.json(response.data);
 
     // Forward the response from the target URL back to the client
-    res.status(response.status).send(response.body);
+    // res.status(response.status).send(await response.text());
   } catch (error) {
-    console.error('Error in proxy:', error);
-    res.status(500).send('Error in proxy');
+    console.error("Error in proxy:", error);
+    res.status(500).send("Error in proxy");
   }
 };
 
